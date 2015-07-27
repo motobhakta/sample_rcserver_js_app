cd0debda51374252dbe6c72d9d19e75f25839e2a
Bhakta Ranjan Satapathy <bhakta@zebra.com>
2015-07-27 13:08:24 +0530

    Update custom_routes_controller.js

--- controllers/js/custom_routes_controller.js
+++ controllers/js/custom_routes_controller.js
@@ -4,8 +4,6 @@ app.controllerName('CustomRoutes');
 
 app.post('/testpost', {}, function(req,resp){
   console.log("testpost hit ...");
-  console.log(req.userName);
-  console.log(req.password);
   console.log(JSON.stringify(req));
   resp.send(
     JSON.stringify({'status': 'ok', 'username': req.params.userName, 'password': req.params.password})
